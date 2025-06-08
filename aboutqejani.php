007f3c05c0a7d00da1667a7da83308e9912a6a33
joyk431 <23zad108351@stu.riarauniversity.ac.ke>
2025-06-08 23:38:46 +0300

    Add files via upload

---
+++ aboutqejani.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>About Us</title>
+  <link rel="stylesheet" type="text/css" href="aboutqejani.css">
+  </head>
+  <body>
+    <?php include("navbar.php"); ?>
+    <main>
+
+
+    
+  <!--ABOUT QEJANI-->
+ <div class="section">
+    <div class="about-flex">
+      <div class="about-image">
+        <img width="500" src="images/qejani-hostels.jpeg">
+      </div>
+      <div class="about-content">
+        <div class="about-head">
+          <h2 style="font-size: 50px"><b>ABOUT QEJANI HOSTELS</b></h2>
+        </div>
+        <p> Qejani Hostels, located in Ongata Rongai, provide quality and affordable student accommodation for
+          learners from Multimedia University, Co-operative University, and Catholic University.
+          We offer reliable transport to and from campus, making daily commutes easy and stress-free. At Qejani,
+          students enjoy a clean, secure, and peaceful environment — perfect for studying, relaxing, and building
+          lasting friendships. With essential amenities and comfortable living spaces, Qejani Hostels truly feel like a
+          home away from home — all at a student-friendly price.</p>
+      </div>
+    </div>
+  </div>
+   <!--MISSION-->
+  <div class="qejani-details">
+    <div class="pic-1">
+      <img width="100" src="images/qejani-mission.png">
+      <h1>Mission</h1>
+      <p>To provide safe, affordable, and student-friendly accommodation
+        that supports academic success, personal growth, and a comfortable living experience.</p>
+    </div>
+    <!--VISION-->
+    <div class="pic-2">
+        <img width="100" src="images/qejani-vision.jpg">
+        <h1>Vision</h1>
+      <p>To be the preferred student hostel in Ongata Rongai and beyond, 
+        known for quality service, comfort, and a supportive community where students thrive.
+      </p>
+    </div>
+    <!--CORE VALUES-->
+    <div class="pic-3">
+       <img src="images/qejani-core-values.png">
+       <h2>Core value</h2>
+      <p>At Qejani Hostels, we value safety, cleanliness, and affordability. 
+        We’re committed to creating a reliable, student-focused environment where residents feel at home and supported in their academic journey.</p>
+    </div>
+  </div>
+  </main>
+  <footer>
+    <div class="footer-bottom">
+      <p>&copy; 2025 Qejani Hostels. All Rights Reserved.</p>
+    </div>
+  </div>
+  </footer>
+  </body>
+</html>
---
+++ home.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link rel="icon" href="images/logo.png" type="image/icon">
+  <link rel="stylesheet" href="Riverside.css">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Rajdhani&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>QEJANI</title>
+  <!-- SweetAlert2 CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+  <!-- SweetAlert2 JS -->
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+  <!-- NAVBAR -->
+  <header class="head">
+    <div id="navbarFlex" class="navbar-flex">
+      <div class="hamburg">
+        <span class="hamburg2" onclick="openNav()">&#9776;</span>
+        <div class="logo">
+          <img width="180" src="images/qejani-logo.png">
+          <p><span><b>QEJANI </b></span> <b>HOSTELS</b></p>
+        </div>
+      </div>
+    </div>
+    <div id="myNav" class="overlay">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      <div class="overlay-content">
+        <a href="http://localhost/RIVERSIDE-HOSTEL-MANAGMENT-SYSTEM/ADMIN/adminportal.php">Admin Portal</a>
+      </div>
+    </div>
+  </header>
+
+  <script>
+    function openNav() {
+      document.getElementById("myNav").style.width = "50%";
+      document.getElementById('imgg').style.display = "none";
+      document.getElementById("navbarFlex").style.zIndex = "0";
+      document.getElementById('text').style.opacity = "none"; // Hide the text element
+    }
+
+    function closeNav() {
+      document.getElementById("myNav").style.width = "0%";
+      document.getElementById("navbarFlex").style.zIndex = "1000";
+      document.getElementById('text').style.display = "block"; // Show the text element again
+    }
+  </script>
+
+  <div class="background-image">
+   <?php include("navbar.php"); ?>
+
+
+
+
+
+    <!-- WELCOME SECTION -->
+    <section id="middle" class="middle">
+      <div id="text" class="text">
+        <h2>WELCOME TO <span style="color: greenyellow"> QEJANI HOSTEL</span></h2>
+        <p>Where Students<br> <span style="color: greenyellow">Feel</span> at Home.</p>
+        <button onclick='rooms()' type="button" class="btn btn-outline-success text-white">
+          <span id="Riverside-about" class="ROOM-NAV">View Rooms</span>
+        </button>
+      </div>
+    </section>
+  </div>
+
