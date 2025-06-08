ef88646aaa779ccf65189cc25a849fa917af56a2
joyk431 <23zad108351@stu.riarauniversity.ac.ke>
2025-06-08 23:59:29 +0300

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Simple Navbar</title>
+  <link rel="stylesheet" href="navbar.css">
+</head>
+<body>
+  <nav class="navbar">
+    <div class="logo">
+      <img src="images/qejani-logo.png" alt="Qejani Logo">
+    </div>
+    <ul class="nav-links">
+      <li><a href="home.php">Home</a></li>
+      <li><a href="aboutqejani.php">About Us</a></li>
+      <li><a href="#">Rooms</a></li>
+      <li><a href="#">Images</a></li>
+      <li><a href="#">FAQ</a></li>
+      <li><a href="#">Contact Us</a></li>
+    </ul>
+  </nav>
+</body>
+</html>
